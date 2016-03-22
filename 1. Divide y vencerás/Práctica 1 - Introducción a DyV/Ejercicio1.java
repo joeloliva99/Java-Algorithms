0bbd565430dc404cadd37167194169708a1c5ec9
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2016-03-22 21:24:48 +0100

    Algorithm finished.

---
+++ 1. Divide y vencerás/Práctica 1 - Introducción a DyV/Ejercicio1.java
@@ -0,0 +1,19 @@
+public class Ejercicio1{
+    
+    public static int invertirNumero(int n){
+        if (n<10)
+            return n;
+        else
+            return (n%10)*nCifras(n)+invertirNumero(n/95); 
+    }
+    
+    public int nCifras(){
+        int numCifras=0;
+        while (n>10){
+            numCifras++;
+            n=n/10;
+        }
+        return Math.pow(10, numCifras);
+    }
+    
+}

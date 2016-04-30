e83e4e3efa5f5382c83c335bbe76a50ea72bdc1d
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2016-04-30 13:22:12 +0200

    Algoritmo

--- 4. Algoritmos voraces/Aplicación 2 - Escoger k elementos/EscogerKElementos.java
+++ 4. Algoritmos voraces/Aplicación 2 - Escoger k elementos/EscogerKElementos.java
@@ -5,35 +5,38 @@ import java.util.List;
 public class EscogerKElementos{
 
     /* ----------------- IMPLEMENTACIÓN DEL ALGORITMO ----------------- */
-    public static int[] escogerkElementos(int k, Integer[] vectorNumeros){
-        ArrayList<Integer> numeros=new ArrayList<Integer>(Arrays.asList(vectorNumeros));
-        int[] solucion=new int[k];
-        int i=0;
-        int j=0;
-        while (i<k && j<numeros.size())
-            if (numeros.get(j)==getElementoMayor(numeros)){
-                solucion[i]=numeros.get(j);
-                numeros.remove(j);
-                j=0;
-                i++;
-            } else
-                j++;
+    public static Integer[] escogerKElementos(int k, Integer[] numeros){
+        Integer[] solucion=new Integer[k];
+        ArrayList<Integer> vectorNumeros=new ArrayList<Integer>(Arrays.asList(numeros));
+        for (int i=0; i<k; i++){
+            int posicion=getPosicionMayorElemento(vectorNumeros);
+            solucion[i]=vectorNumeros.get(posicion);
+            vectorNumeros.remove(posicion);
+        }
         return solucion;
     }
     
-    private static int getElementoMayor(ArrayList<Integer> numeros){
-        int elemento=0;
-        for (int i=0; i<numeros.size(); i++){
-            if (elemento<numeros.get(i))
-                elemento=numeros.get(i);
+    public static int getPosicionMayorElemento(ArrayList<Integer> numeros){
+        int valor=numeros.get(0);
+        int posicion=0;
+        for (int i=1; i<numeros.size(); i++){
+            if (valor<numeros.get(i)){
+                valor=numeros.get(i);
+                posicion=i;
+            }
         }
-        return elemento;
+        return posicion;
     }
     
     /* --------------------- PRUEBA DEL ALGORITMO --------------------- */
     public static void main(String[] args){
-        Integer[] vector={10, 2, 5, 7, 3, 4};
-        System.out.println(Arrays.toString(escogerkElementos(4, vector)));
+        int k=10;
+        Integer[] numeros=new Integer[k];
+        for (int i=0; i<k; i++){
+            numeros[i]=(int) (Math.random()*20+1);
+        }
+        System.out.println(Arrays.toString(numeros));
+        System.out.println(Arrays.toString(escogerKElementos(3, numeros)));
     }
     
 }
